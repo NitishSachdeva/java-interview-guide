0f6f82bcba5c215938269216b71ce45936dff961
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-14 23:41:50 +0530

    First Pattern program

---
+++ src/star_patterns/Pattern01.java
@@ -0,0 +1,20 @@
+/**
+ * Java program to print below patterns
+ *          *
+ *          **
+ *          ***
+ *          ****
+*/
+
+package star_patterns;
+
+public class Pattern01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
